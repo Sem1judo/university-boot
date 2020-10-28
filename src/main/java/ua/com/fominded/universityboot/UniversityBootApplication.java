ece1979add301b6c60a6349cb0046f78f61e90ba
Stanislav <judo1stss1@gmail.com>
2020-10-28 20:15:06 +0200

    1

---
+++ src/main/java/ua/com/fominded/universityboot/UniversityBootApplication.java
@@ -0,0 +1,13 @@
+package ua.com.fominded.universityboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/fominded/universityboot/UniversityBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.fominded.universityboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
